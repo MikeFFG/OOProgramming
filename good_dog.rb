fc6a878e2fdea67e2922fb312390e4f805ad6038
Michael Burke <mburke@izotope.com>
2016-05-19 15:14:27 -0400

    work on States and Behaviors.

--- good_dog.rb
+++ good_dog.rb
@@ -1,19 +1,29 @@
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name, :height, :weight
 
-  def initialize(name)
-    @name = name
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
-    "#{@name} says arf!"
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
   end
 end
 
-fido = GoodDog.new("Fido")
-sparky = GoodDog.new("Sparky")
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info      # => Sparky weighs 10 lbs and is 12 inches tall.
 
-puts sparky.speak
-puts sparky.name
-sparky.name = "Spartacus"
-puts sparky.name
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info      # => Spartacus weighs 45 lbs and is 24 inches tall.
