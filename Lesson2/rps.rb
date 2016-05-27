2e3a6d954396de079a25a5241c0bf7d867df7961
Michael Burke <mike.d.burke@gmail.com>
2016-05-27 19:52:27 -0400

    Finish display winner method for rps.

--- Lesson2/rps.rb
+++ Lesson2/rps.rb
@@ -1,27 +1,92 @@
 class Player
-  def initialize
-    # maybe a "name"? what about a "move"?
+  attr_accessor :move
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
   end
 
   def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, or scissors:"
+        choice = gets.chomp
+        break if ['rock', 'paper', 'scissors'].include? choice
+        puts "Sorry, invalid choice."
+      end
+      self.move = choice
+    else
+      self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
 
+  def human?
+    @player_type == :human
   end
 end
 
-class Move
+# class Move
+#   def initialize
+#     # seems like we need something to keep track
+#     # of the choice... a move object can be "paper", "rock" or "scissors"
+#   end
+# end
+
+# class Rule
+#   def intitialize
+#     # not sure what the "state" of a rule object should be
+#   end
+# end
+
+# # not sure where "compare" goes yet
+# def compare(move1, move2)
+
+# end
+
+class RPSGame
+  attr_accessor :human, :computer
+
   def initialize
-    # seems like we need something to keep track
-    # of the choice... a move object can be "paper", "rock" or "scissors"
+    @human = Player.new
+    @computer = Player.new(:computer)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
+  end
+
+  def display_winner
+    puts "You chose #{human.move}."
+    puts "The computer chose #{computer.move}."
+
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts "You won!" if computer.move == 'scissors'
+      puts "You lose" if computer.move == 'paper'
+    when 'scissors'
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts "You won!" if computer.move == 'paper'
+      puts "You lose" if computer.move == 'rock'
+    when 'paper'
+      puts "It's a tie!" if computer.move == 'paper'
+      puts "You won!" if computer.move == 'rock'
+      puts "You lose" if computer.move == 'scissors'
+    end
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_winner
+    display_goodbye_message
   end
 end
 
-class Rule
-  def intitialize
-    # not sure what the "state" of a rule object should be
-  end
-end
-
-# not sure where "compare" goes yet
-def compare(move1, move2)
-
-end
+RPSGame.new.play
