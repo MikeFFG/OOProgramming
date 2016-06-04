cea63d75e7c63b03b602453017c44b9a2c8ca6d7
Michael Burke <mike.d.burke@gmail.com>
2016-06-04 10:30:27 -0400

    Finish OOP twenty_one

--- lesson5/twenty_one.rb
+++ lesson5/twenty_one.rb
@@ -6,8 +6,8 @@ module Displayable
     system('clear') || system('cls')
   end
 
-  def prompt(msg)
-    puts "=> #{msg}"
+  def prompt(message)
+    puts "=> #{message}"
   end
 
   def display_welcome_message
@@ -32,6 +32,7 @@ module Displayable
                       "#{hand[index]} "
                     end
     end
+
     new_string
   end
 
@@ -48,6 +49,14 @@ module Displayable
     prompt "==============="
   end
 
+  def display_total
+    prompt "#{name}'s total is #{total}"
+  end
+
+  def display_hit
+    hand.last.to_s
+  end
+
   def hit_or_stay
     prompt "Hit or Stay? (h or s)"
     answer = nil
@@ -56,6 +65,48 @@ module Displayable
       break if ['h', 's'].include?(answer)
       prompt "Invalid entry. Enter 'h' or 's' only."
     end
+    answer
+  end
+
+  def display_cards
+    string_to_display = "#{name} shows: " + concatenate_cards(hand)
+    prompt string_to_display
+  end
+
+  def display_initial_cards
+    dealer.display_initial_cards
+    player.display_cards
+  end
+
+  def display_result
+    display_string = case detect_result
+                     when :player_busted
+                       "#{player.name} busted! #{dealer.name} wins this round!"
+                     when :player_won
+                       "#{player.name} wins the round!"
+                     when :dealer_busted
+                       "#{dealer.name} busted! #{player.name} wins this round!"
+                     when :dealer_won
+                       "#{dealer.name} wins the round!"
+                     when :push
+                       "It's a push!"
+                     end
+    prompt display_string
+    prompt "==============="
+  end
+
+  def display_score
+    prompt "Current score is:"
+    prompt "#{player.name}: #{player.score}"
+    prompt "#{dealer.name}: #{dealer.score}"
+    prompt "==============="
+  end
+
+  def display_match_winner
+    winner = player.score == 5 ? player : dealer
+    prompt ""
+    prompt "#{winner.name.upcase} WINS THE MATCH!"
+    prompt ""
   end
 end
 
@@ -71,10 +122,6 @@ class Participant
 
   def hit(card)
     hand << card
-  end
-
-  def display_hit
-    hand.last.to_s
   end
 
   def total
@@ -98,21 +145,12 @@ class Participant
     sum
   end
 
-  def display_total
-    prompt "#{name}'s total is #{total}"
-  end
-
   def busted?
     total > 21
   end
 
   def reset_hand
     self.hand = []
-  end
-
-  def display_cards
-    string_to_display = "#{name} shows: " + concatenate_cards(hand)
-    prompt string_to_display
   end
 end
 
@@ -123,8 +161,8 @@ class Player < Participant
 
   def turn(deck)
     display_turn_start
-
     loop do
+      answer = hit_or_stay
       clear_screen
       if answer == 's'
         prompt "#{name} stays!"
@@ -172,11 +210,7 @@ end
 class Deck
   def initialize
     @cards = []
-    Card::SUITS.each do |suit|
-      Card::FACES.each do |face|
-        @cards << Card.new(face, suit)
-      end
-    end
+    reset
     shuffle!
   end
 
@@ -185,7 +219,16 @@ class Deck
   end
 
   def deal
+    reset if @cards.size < 10
     @cards.shift
+  end
+
+  def reset
+    Card::SUITS.each do |suit|
+      Card::FACES.each do |face|
+        @cards << Card.new(face, suit)
+      end
+    end
   end
 end
 
@@ -223,28 +266,6 @@ class Game
     dealer.hand[1] = deck.deal
   end
 
-  def display_initial_cards
-    dealer.display_initial_cards
-    player.display_cards
-  end
-
-  def display_result
-    display_string = case detect_result
-                     when :player_busted
-                       "#{player.name} busted! #{dealer.name} wins this round!"
-                     when :player_won
-                       "#{player.name} won!"
-                     when :dealer_busted
-                       "#{dealer.name} busted! #{player.name} wins this round!"
-                     when :dealer_won
-                       "#{dealer.name} won!"
-                     when :push
-                       "It's a push!"
-                     end
-    prompt display_string
-    prompt "==============="
-  end
-
   def detect_result
     player_total = player.total
     dealer_total = dealer.total
@@ -268,21 +289,15 @@ class Game
     end
   end
 
-  def display_score
-    prompt "Current score is:"
-    prompt "#{player.name} #{player.score}"
-    prompt "#{dealer.name} #{dealer.score}"
-    prompt "==============="
-  end
-
-  def next_round?
-    prompt "Ready for the next round? (y or n)"
+  def next_game?(type)
+    prompt "Ready for a new #{type}? (y or n)"
     answer = nil
     loop do
       answer = gets.chomp.downcase
       break if ['y', 'n'].include?(answer)
       prompt "Invalid entry. Enter 'y' or 'n' only."
     end
+
     return true if answer == 'y'
     false
   end
@@ -309,18 +324,50 @@ class Game
   end
   # rubocop:enable Metrics/AbcSize
 
+  def match_winner?
+    player.score == 5 || dealer.score == 5
+  end
+
+  def early_quit
+    prompt "You didn't finish the match. Do you want to start again? (y or n)"
+    answer = nil
+    loop do
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include?(answer)
+      prompt "Invalid entry. Please enter only 'y' or 'n'"
+    end
+    answer
+  end
+
+  def reset_match
+    reset_hands
+    player.score = 0
+    dealer.score = 0
+    deck.reset
+  end
+
   def start
     clear_screen
     display_welcome_message
     display_press_key_to_start
-
     loop do
-      clear_screen
-      play_single_round
-      reset_hands
-      display_score
-      break unless next_round?
+      loop do
+        clear_screen
+        play_single_round
+        reset_hands
+        display_score
+        break if match_winner?
+        break unless next_game?(:round)
+      end
+      if match_winner?
+        display_match_winner
+        break unless next_game?(:match)
+      elsif early_quit == 'n'
+        break
+      end
+      reset_match
     end
+
     prompt "Thanks for playing Twenty-One. Goodbye!"
   end
 end
